04505f4fb37a0b700c04756ca0a9698e678d3fa4
loudringphone <loudringphone@gmail.com>
2023-02-20 15:36:55 +1100

    logout button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :fetch_admin
+    
+    def fetch_admin
+        @current_admin = Admin.find_by :id => session[:admin_id] if session[:admin_id].present?
+        session[:admin_id] = nil unless @current_admin.present?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,12 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/edit'
   get 'users/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root :to => 'pages#home'
+  get '/users' => 'pages#users'
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+  
 end
