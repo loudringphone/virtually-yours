701b4fcc19d2c0d29648e3267c4cc6c3075f02ae
loudringphone <loudringphone@gmail.com>
2023-02-21 00:07:48 +1100

    css for buttons

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy'
+  get '/login' => 'session#new'
 
 end
